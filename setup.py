b73d6c414a8740d7e860548b8b417b7281ccd2dd
Seth Parker <csparker247@gmail.com>
2020-07-01 19:15:41 -0400

    Fix bug where module had to be imported as exiftool.exiftool.

--- setup.py
+++ setup.py
@@ -31,4 +31,5 @@ setup(  name="PyExifTool",
 			"Programming Language :: Python :: 2.7",
 			"Programming Language :: Python :: 3",
 			"Topic :: Multimedia"],
-		py_modules=["exiftool/exiftool"])
+		package_dir={'exiftool': 'exiftool'},
+		packages=['exiftool'])
